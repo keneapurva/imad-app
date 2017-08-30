b4a442bcbabf81ff44a726399db494162f70a552
keneapurva <31405879+keneapurva@users.noreply.github.com>
2017-08-30 23:15:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,11 +32,9 @@ function createTemplate(data){
             <title>
                 ${title}
             </title>
-            <meta name="viewport" content="width-device-width,initial-scale=1">
+            <meta name="viewport" content="width-device-width,initial-scale=1"/>
             <link href="/ui/style.css" rel="stylesheet" />
-            <style>
             
-            </style>
         </head>
         <body>
             <div class="container">
